35e9cb4bc31b8d956569ef3cd55ae12e1878a47a
jbub <contact@jbub.eu>
2015-02-25 11:50:17 +0100

    update doc file

--- doc.go
+++ doc.go
@@ -1,7 +1,64 @@
-// Copyright (c) 2014 Juraj Bubniak.
-
 /*
 Package banking provides banking utilities.
-*/
 
+IBAN:
+
+    package main
+
+    import (
+        "fmt"
+        "log"
+
+        "github.com/jbub/banking/iban"
+    )
+
+    var (
+        testIban = iban.MustParse("BE68539007547034")
+    )
+
+    func main() {
+        ibn, err := iban.New("BE68539007547034")
+        if err != nil {
+            log.Fatal(err)
+        }
+
+        fmt.Println(ibn.BankCode())
+        fmt.Println(ibn.AccountNumber())
+
+        err = iban.Validate("BE68539007547034")
+        if err != nil {
+            log.Fatal(err)
+        }
+    }
+
+Swift:
+
+    package main
+
+    import (
+        "fmt"
+        "log"
+
+        "github.com/jbub/banking/swift"
+    )
+
+    var (
+        testSwift = swift.MustParse("DEUTDEFF500")
+    )
+
+    func main() {
+        swft, err := swift.New("DEUTDEFF500")
+        if err != nil {
+            log.Fatal(err)
+        }
+
+        fmt.Println(swft.BankCode())
+        fmt.Println(swft.CountryCode())
+
+        err = swift.Validate("DEUTDEFF500")
+        if err != nil {
+            log.Fatal(err)
+        }
+    }
+*/
 package banking
