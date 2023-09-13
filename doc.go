8a32c2a3a3fc7ddb31b7777e68220c2cfe17255d
Juraj Bubniak <juraj.bubniak@gmail.com>
2023-09-13 09:23:51 +0200

    Use Go 1.21.

--- doc.go
+++ doc.go
@@ -3,62 +3,62 @@ Package banking provides banking utilities.
 
 IBAN:
 
-    package main
+	package main
 
-    import (
-        "fmt"
-        "log"
+	import (
+	    "fmt"
+	    "log"
 
-        "github.com/jbub/banking/iban"
-    )
+	    "github.com/jbub/banking/iban"
+	)
 
-    var (
-        testIban = iban.MustParse("BE68539007547034")
-    )
+	var (
+	    testIban = iban.MustParse("BE68539007547034")
+	)
 
-    func main() {
-        ibn, err := iban.Parse("BE68539007547034")
-        if err != nil {
-            log.Fatal(err)
-        }
+	func main() {
+	    ibn, err := iban.Parse("BE68539007547034")
+	    if err != nil {
+	        log.Fatal(err)
+	    }
 
-        fmt.Println(ibn.BankCode())
-        fmt.Println(ibn.AccountNumber())
+	    fmt.Println(ibn.BankCode())
+	    fmt.Println(ibn.AccountNumber())
 
-        err = iban.Validate("BE68539007547034")
-        if err != nil {
-            log.Fatal(err)
-        }
-    }
+	    err = iban.Validate("BE68539007547034")
+	    if err != nil {
+	        log.Fatal(err)
+	    }
+	}
 
 Swift:
 
-    package main
+	package main
 
-    import (
-        "fmt"
-        "log"
+	import (
+	    "fmt"
+	    "log"
 
-        "github.com/jbub/banking/swift"
-    )
+	    "github.com/jbub/banking/swift"
+	)
 
-    var (
-        testSwift = swift.MustParse("DEUTDEFF500")
-    )
+	var (
+	    testSwift = swift.MustParse("DEUTDEFF500")
+	)
 
-    func main() {
-        swft, err := swift.Parse("DEUTDEFF500")
-        if err != nil {
-            log.Fatal(err)
-        }
+	func main() {
+	    swft, err := swift.Parse("DEUTDEFF500")
+	    if err != nil {
+	        log.Fatal(err)
+	    }
 
-        fmt.Println(swft.BankCode())
-        fmt.Println(swft.CountryCode())
+	    fmt.Println(swft.BankCode())
+	    fmt.Println(swft.CountryCode())
 
-        err = swift.Validate("DEUTDEFF500")
-        if err != nil {
-            log.Fatal(err)
-        }
-    }
+	    err = swift.Validate("DEUTDEFF500")
+	    if err != nil {
+	        log.Fatal(err)
+	    }
+	}
 */
 package banking
